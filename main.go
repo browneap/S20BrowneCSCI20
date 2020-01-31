a659622d1834e40a8286a0b151096c4de2dd8d07
browneap <browneap@butte.edu>
2020-01-31 03:18:57 +0000

    Updated for boolean

--- main.go
+++ main.go
@@ -3,5 +3,24 @@ package main
 import "fmt"
 
 func main() {
-  fmt.Println("Hello World")
+  
+  fmt.Println("1+1=", 1+1)
+
+  fmt.Println("7/3", 7/3)
+  fmt.Println("7.0/3.0", 7.0/3.0)
+  
+  fmt.Println("AND")
+  fmt.Println(true && false)
+  fmt.Println(true && true)
+  fmt.Println(false && false)
+
+  fmt.Println("OR")
+  fmt.Println(true || false)
+  fmt.Println(true || true)
+  fmt.Println(false || false)
+
+  fmt.Println("NOT")
+  fmt.Println(!true)
+  fmt.Println(!false)
+
 }
